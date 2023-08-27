c72923c94ac3d885382c8f631ebfd67ed484f5de
zinged5 <33462258+zinged5@users.noreply.github.com>
2023-08-27 14:03:06 +0100

    Initial commit

---
+++ src/test/java/com/org/eureka/directory/DirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.eureka.directory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
